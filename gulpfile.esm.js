d0a695ef729f3d52e89d43a574e3a4622e91504b
Lakshmikanta Patra <frontendfixer@gmail.com>
2022-12-24 14:56:54 +0530

    new folder structure for distribution mode

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,53 +1,58 @@
 import gulp from 'gulp';
-const { src, dest, watch, series } = gulp;
 import changed from 'gulp-changed';
 
 import htmlminifier from 'gulp-html-minifier-terser';
 
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass);
-import GulpPostCss from 'gulp-postcss';
 import autoPrefixer from 'autoprefixer';
 import cssnano from 'cssnano';
+import GulpPostCss from 'gulp-postcss';
+import gulpSass from 'gulp-sass';
 import postcssPresetEnv from 'postcss-preset-env';
+import dartSass from 'sass';
 
 import babel from 'gulp-babel';
 import GulpConcat from 'gulp-concat';
 import GulpTerser from 'gulp-terser';
 
+// +++++++++ SVG Start ++++++++++
+import svgSprite from 'gulp-svg-sprite';
+import svgMin from 'gulp-svgo';
+
+const { src, dest, watch, series } = gulp;
+const sass = gulpSass(dartSass);
+
 const browsersync = require('browser-sync').create();
 
-//+++++++++ PATH ++++++++++++
+// +++++++++ PATH ++++++++++++
 const path = {
   html: {
     src: './src/index.html',
     dest: './build/',
-    pro: './dist',
+    pro: '.',
   },
   css: {
     src: ['./src/assets/scss/style.scss'],
     dest: './build/assets/css/',
-    pro: './dist/assets/css/',
+    pro: './assets/css/',
   },
 
   js: {
     src: [
       './src/assets/js/dom-selector.js',
       './src/assets/js/component.js',
-      // './src/assets/js/sprite.js',
+      './src/assets/js/sprite.js',
       './src/assets/js/main.js',
     ],
     dest: './build/assets/js/',
-    pro: './dist/assets/js/',
+    pro: './assets/js/',
   },
   img: {
     srcJpgPng: ['./src/assets/img/*.{jpg,png}'],
-    srcSvg: ['./src/assets/img/*.svg'],
+    srcSvg: ['./src/assets/img/SVG/*.svg'],
     destJpgPng: './build/assets/img/',
     destSvg: './build/assets/img/SVG/',
-    proJpgSvg: './dist/assets/img/',
-    proSvg: './dist/assets/img/SVG',
+    proJpgSvg: './assets/img/',
+    proSvg: './assets/img/SVG',
   },
   screenshot: {
     src: './src/screenshot/*.{jpg,png}',
@@ -55,10 +60,6 @@ const path = {
     pro: './dist/assets/screenshot/',
   },
 };
-
-// +++++++++ SVG Start ++++++++++
-import svgMin from 'gulp-svgo';
-import svgSprite from 'gulp-svg-sprite';
 
 const config = {
   mode: {
@@ -111,7 +112,7 @@ export function htmlPro() {
         minifyJS: true,
         preserveLineBreaks: false,
         removeComments: true,
-      })
+      }),
     )
     .pipe(dest(path.html.pro));
 }
@@ -132,39 +133,63 @@ const plugins = [
   }),
 ];
 export function cssTask() {
-  return src(path.css.src, { sourcemaps: true })
+  return src(path.css.src, {
+    sourcemaps: true,
+  })
     .pipe(changed(path.css.dest))
     .pipe(sass())
-    .pipe(dest(path.css.dest, { sourcemaps: '.' }));
+    .pipe(
+      dest(path.css.dest, {
+        sourcemaps: '.',
+      }),
+    );
 }
 
 export function cssPro() {
-  return src(path.css.src, { sourcemaps: true })
+  return src(path.css.src, {
+    sourcemaps: true,
+  })
     .pipe(changed(path.css.dest))
     .pipe(sass().on('error', sass.logError))
     .pipe(GulpPostCss(plugins))
-    .pipe(dest(path.css.pro, { sourcemaps: '.' }));
+    .pipe(
+      dest(path.css.pro, {
+        sourcemaps: '.',
+      }),
+    );
 }
 
 // +++++++++ javaScript Start ++++++++++++
 export function jsTask() {
-  return src(path.js.src, { sourcemaps: true })
+  return src(path.js.src, {
+    sourcemaps: true,
+  })
     .pipe(changed(path.js.dest))
     .pipe(GulpConcat('script.js'))
-    .pipe(dest(path.js.dest, { sourcemaps: '.' }));
+    .pipe(
+      dest(path.js.dest, {
+        sourcemaps: '.',
+      }),
+    );
 }
 
 export function jsPro() {
-  return src(path.js.src, { sourcemaps: true })
+  return src(path.js.src, {
+    sourcemaps: true,
+  })
     .pipe(changed(path.js.dest))
     .pipe(GulpConcat('script.js'))
     .pipe(
       babel({
         presets: ['@babel/preset-env'],
-      })
+      }),
     )
     .pipe(GulpTerser())
-    .pipe(dest(path.js.pro, { sourcemaps: '.' }));
+    .pipe(
+      dest(path.js.pro, {
+        sourcemaps: '.',
+      }),
+    );
 }
 
 // +++++++++ Browse-sync Start ++++++++++++
@@ -185,25 +210,22 @@ function browsersyncReload(cb) {
   cb();
 }
 
-//+++++++++ Watch Task +++++++++++
+// +++++++++ Watch Task +++++++++++
 function watchTask() {
   watch(
     ['./src/*.html', './src/assets/scss/**/*.scss', './src/assets/js/**/*.js'],
-    series(htmlTask, cssTask, jsTask, browsersyncReload)
+    series(htmlTask, cssTask, jsTask, browsersyncReload),
   );
 }
 
-// Gulp Image Processing
-export const image = series(svgTask, imgTask);
-
 // Default Gulp task
 const build = series(
-  gulp.parallel(image, htmlTask, cssTask, jsTask, browsersyncServe),
-  watchTask
+  gulp.parallel(svgTask, imgTask, htmlTask, cssTask, jsTask, browsersyncServe),
+  watchTask,
 );
 export default build;
 
 // Distribution Gulp Task
 export const dist = series(
-  gulp.parallel(svgPro, imgPro, htmlPro, cssPro, jsPro)
+  gulp.parallel(svgPro, imgPro, htmlPro, cssPro, jsPro),
 );
